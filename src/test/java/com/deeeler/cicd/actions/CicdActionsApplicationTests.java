27222b5df293679c30fbf499d6876bf7a7ca67eb
dlimiyassin <dlimiyassine08@gmail.com>
2024-06-04 10:43:35 +0100

    code

---
+++ src/test/java/com/deeeler/cicd/actions/CicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeeler.cicd.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
